327e1667d115c715733f41e2489a8c22478eb501
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-09-23 13:15:15 +0100

    changes

--- src/content.ts
+++ src/content.ts
@@ -9,28 +9,33 @@ import {
 let extraTextList: string[] = [];
 
 const main = () => {
-  const elements = getElemetList();
+  const elementsArr = getElemetList();
+  const latest10Elemets = elementsArr.slice(
+    Math.max(elementsArr.length - 10, 0)
+  );
 
-  elements.forEach(async (element) => {
+  latest10Elemets.forEach(async (element) => {
     const text = getText(element as Element);
     const name = getName(element as Element);
     const meta = getMetaData(element as Element);
 
-    const extraText =
-      name && meta
-        ? `${name}: ${meta}\n${"-".repeat(1 + name.length + meta.length)}`
-        : "";
-    if (text && !extraTextList.includes(extraText)) {
-      console.log(
-        `
-    ${extraText}
-    ${text}
-      `
-      );
+    // const extraText =
+    //   name && meta
+    //     ? `${name}: ${meta}\n${"-".repeat(1 + name.length + meta.length)}`
+    //     : "";
+    const identifer = `${meta}${text}`;
+    if (text && !extraTextList.includes(identifer)) {
+      extraTextList.push(identifer);
+      //   console.log(
+      //     `
+      // ${extraText}
+      // ${text}
+      //   `
+      //   );
+      console.log(extraTextList);
       try {
         const res = await postData(text, name, meta);
       } catch (error) {}
-      extraTextList.push(extraText);
     }
   });
 };
